8d7aa75c583d710fd2ea9266a16d4a5a3cd66a2e
Gatovsky <101161255+hharieta@users.noreply.github.com>
2023-11-17 03:20:56 -0500

    Flask has been incorporated as a backend framework

---
+++ pages.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, render_template
+
+pages_bp = Blueprint('pages', __name__, template_folder='templates', static_folder='static')
+
+
+@pages_bp.route('/')
+def index():
+    return render_template('index.html')
+
+@pages_bp.route('/menus')
+def menus():
+    return render_template('menus.html')
+
+@pages_bp.route('/resturant')
+def restaurant():
+    return render_template('restaurant.html')
+
+@pages_bp.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@pages_bp.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@pages_bp.route('/contac')
+def contac():
+    return render_template('contac.html')
+
+@pages_bp.route('/about')
+def about():
+    return render_template('about.html')
+
+@pages_bp.route('/filos')
+def filos():
+    return render_template('filos.html')
+
+@pages_bp.route('/registro')
+def registro():
+    return render_template('registro.html')
