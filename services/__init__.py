7aa8e0f4447bf2f169f82ebe24831739ecc8fea8
Gatovsky <101161255+hharieta@users.noreply.github.com>
2023-11-30 03:51:53 -0500

    Now users registre is working properly

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,5 @@
-from .users import UserLogin, CheckLogin, create, update, delete, read_all, read_one
+from .users import UserLogin, CheckLogin, UserRegister, create, update, delete, read_all, read_one
 
 __all__ = [
-    UserLogin, CheckLogin, create, update, delete, read_all, read_one
+    UserLogin, CheckLogin, UserRegister, create, update, delete, read_all, read_one
 ]
